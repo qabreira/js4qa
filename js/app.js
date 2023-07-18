587b4669c66d08d7a5a9e9e914c629e62da6bec4
Qabreira <cabreir@gmail.com>
2023-07-17 22:44:47 -0300

    conhecendo váriáveis em js e criando scripts integrados com o html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('olá, javascript')
+
+var userName = 'meu usuário favorito'
+document.getElementById('user-name').innerHTML = userName
+console.log('o nome do usuário é preenchido pelo scrip js com o valor da variável "userName"')
+
+var nome = 'Mestre Yoda'
+var idade = 100
+var jedi = true
+
+console.log(`a variável "${nome}" é do tipo "${typeof nome}"`)
+console.log(`a variável "${idade}" é do tipo "${typeof idade}"`)
+console.log(`a variável "${jedi}" é do tipo "${typeof jedi}"`)
